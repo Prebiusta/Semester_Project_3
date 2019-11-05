c885cc109ac12208d921a86ab784514808d87078
Prebiusta <davidko123fr@gmail.com>
2019-11-05 22:26:20 +0100

    Added Client main structure

---
+++ SEP3 Client/WebCoreMVC.NET/Controllers/ListController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebCoreMVC.NET.Models;
+
+namespace WebCoreMVC.NET.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Person()
+        {
+            List<Person> people = new List<Person>();
+            Person person = new Person("Borys", "Birthday", "Male", "Bottle of vodka");
+            Person person2 = new Person("Roxanne", "12.02.1992", "Female", "200 euro per hour");
+            people.Add(person);
+            people.Add(person2);
+            return View(people);
+        }
+    }
+}
---
+++ SEP3 Client/WebCoreMVC.NET/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebCoreMVC.NET.Models;
+
+namespace WebCoreMVC.NET.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly HttpClient client = new HttpClient();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Authorization(SystemUser user)
+        {
+            /*string orgMsg = "dickhead";
+            string otherMsg = "ilovemymom";
+            Console.WriteLine("HMAC SHA256 Demo in .NET");
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+            var salt = MyHmac.GenerateSalt();
+            var hmac1 = MyHmac.ComputeHMAC_SHA256(Encoding.UTF8.GetBytes(orgMsg), salt);
+            var hmac2 = MyHmac.ComputeHMAC_SHA256(Encoding.UTF8.GetBytes(otherMsg), salt);
+            Console.WriteLine("Hashing of " + "dickhead:{0}", Convert.ToBase64String(hmac1));
+            Console.WriteLine("Hashing of " + "ilovemymom99:{0}", Convert.ToBase64String(hmac2));
+            */
+            if(ModelState.IsValid)
+            {
+                WebRequest request = WebRequest.Create("http://10.152.220.131:8080/api/task");
+                request.Method = "GET";
+
+
+
+
+                return RedirectToAction("Index", "Home");
+            } else {           
+                ModelState.AddModelError(string.Empty, "Please insert the necessary data");
+                return View("Index");
+            }
+
+            //How to confuse a browser 101
+            //return View("../Home/Index");
+            
+        }
+    }
+}
