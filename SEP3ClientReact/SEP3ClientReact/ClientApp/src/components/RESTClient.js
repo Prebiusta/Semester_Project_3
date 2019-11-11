3755e5eddb0e6319084d3bba3ecd2f9511523403
Prebiusta <davidko123fr@gmail.com>
2019-11-11 08:46:37 +0100

    Renamed files

---
+++ SEP3ClientReact/SEP3ClientReact/ClientApp/src/components/RESTClient.js
@@ -0,0 +1,24 @@
+﻿var request = new XMLHttpRequest();
+var serverUrl = "http://192.168.87.103:8080/";
+
+function Post(jsonData, PostWhere) {
+    request.open('POST', serverUrl + PostWhere, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            alert("POST successful");
+            return true;
+        } else {
+            alert("POST not successful");
+            return false;
+        }
+    }
+    request.send(jsonData);
+}
+
+function Get(FromWhere) {
+    request.open('GET', serverUrl + FromWhere, true);
+    request.onload = function () {
+        return request.responseText;
+    }
+}
