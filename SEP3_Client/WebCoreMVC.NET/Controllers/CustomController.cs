993fa39bf4912dcf94ff05255b0c452c34484135
Krystian <279965@via.dk>
2019-11-12 14:46:55 +0100

    hashed values, working skeleton

--- SEP3_Client/WebCoreMVC.NET/Controllers/CustomController.cs
+++ SEP3_Client/WebCoreMVC.NET/Controllers/CustomController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +14,38 @@ namespace WebCoreMVC.NET.Controllers {
         protected readonly HttpClient client = new HttpClient();
         protected readonly string serverAPIurl = "http://" + "10.152.204.51" + ":8080/";
     
-    public async Task<HttpResponseMessage> PostData(Object data, string PostWhere)
+        public async Task<HttpResponseMessage> PostData(Object data, string PostWhere)
         {
-            string json = JsonConvert.SerializeObject(data);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(serverAPIurl + PostWhere, content);
-            return response;
+                string json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(serverAPIurl + PostWhere, content);
+                return response;
+        }
+   
+
+        public static string GetSha256(string text)
+        {
+            if (text == null)
+            {
+                return string.Empty;
+            }
+
+            byte[] message = System.Text.Encoding.ASCII.GetBytes(text);
+            byte[] hashValue = GetSha256(message);
+
+            string hashString = string.Empty;
+            foreach (byte x in hashValue)
+            {
+                hashString += string.Format("{0:x2}", x);
+            }
+            return hashString;
+
+        }
+
+        private static byte[] GetSha256(byte[] message)
+        {
+            SHA256Managed hashString = new SHA256Managed();
+            return hashString.ComputeHash(message);
         }
     }
 }
--- SEP3_Client/WebCoreMVC.NET/Controllers/LoginController.cs
+++ SEP3_Client/WebCoreMVC.NET/Controllers/LoginController.cs
@@ -67,26 +67,7 @@ namespace WebCoreMVC.NET.Controllers
             return variable;
            
         }
-        public static string GetSha256(string text) {
-            if(text==null) {
-                return string.Empty;
-            }
-
-            byte[] message = System.Text.Encoding.ASCII.GetBytes(text);
-            byte[] hashValue = GetSha256(message);
-
-            string hashString = string.Empty;
-            foreach(byte x in hashValue) {
-                hashString+=string.Format("{0:x2}", x);
-            }
-            return hashString;
-
-        }
-
-        private static byte[] GetSha256(byte[] message) {
-            SHA256Managed hashString = new SHA256Managed();
-            return hashString.ComputeHash(message);
-        }
+        
 
     }
 }
