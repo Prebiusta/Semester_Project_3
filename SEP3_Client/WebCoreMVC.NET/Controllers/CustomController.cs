350b4f3d63ec38ee932288288a241f172b3dea8c
Krystian <279965@via.dk>
2019-12-02 22:06:56 +0100

    Update CustomController.cs

--- SEP3_Client/WebCoreMVC.NET/Controllers/CustomController.cs
+++ SEP3_Client/WebCoreMVC.NET/Controllers/CustomController.cs
@@ -4,13 +4,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using WebCoreMVC.NET.Models;
 
 namespace WebCoreMVC.NET.Controllers {
     public class CustomController : Controller {
         //All controllers are reusing the same http client and server ip so there is no need to create them multiple times
         protected readonly HttpClient client = new HttpClient();
         protected readonly string serverUrl = "http://" + "localhost" + ":8081/";
-        protected string username;
+        public static string username { get; set; }
+        protected ClientData clientData = new ClientData();
 
         //Generic Post and Get methods
         public async Task<HttpResponseMessage> PostData(object data, string PostWhere) {
