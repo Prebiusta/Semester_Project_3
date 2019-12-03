95f4423031720d42953b19db15f403be4703d7e3
valerarusu7 <280167@via.dk>
2019-12-03 18:40:27 +0100

    now you can get user stories

--- SEP3_Client/WebCoreMVC.NET/Controllers/MembersController.cs
+++ SEP3_Client/WebCoreMVC.NET/Controllers/MembersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +16,35 @@ namespace WebCoreMVC.NET.Controllers {
             var list = GetUsersInProjects(id).Result;
             var result = JsonConvert.DeserializeObject<List<User>>(list);
             return View("~/Views/Project/Members/Index.cshtml",result);
+        }
+
+        public IActionResult AddMember() {
+            return View("~/Views/Project/Members/AddMember.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult PostMember(User user) {
+            if(ModelState.IsValid) {
+                var response = SendMemberData(user).Result;
+                switch(response.StatusCode) {
+                    case HttpStatusCode.OK:
+                        return RedirectToAction("Index", "Members");
+                    case HttpStatusCode.BadRequest:
+                        ModelState.AddModelError(string.Empty, "Server sent a bad request: " + response.Content);
+                        return AddMember();
+                    default:
+                        ModelState.AddModelError(string.Empty, "Server is not answering");
+                        return AddMember();
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Please insert the necessary data");
+            return AddMember();
+        }
+
+        public async Task<HttpResponseMessage> SendMemberData(User user) {
+            var httpContent = await PostData(user, "api/usersInProject=" + username);
+            return httpContent;
         }
 
         public async Task<string> GetUsersInProjects(int id) {
